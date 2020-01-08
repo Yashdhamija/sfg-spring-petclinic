feac2dddcc3e017629bde8b1a0eb700ca1252c7d
Yash Dhamija <yashdhamija4@gmail.com>
2020-01-08 14:27:32 -0500

    initial project ad

---
+++ src/main/java/com/google/sfgspringpetclinic/SfgSpringPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.google.sfgspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgSpringPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgSpringPetclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/google/sfgspringpetclinic/SfgSpringPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.sfgspringpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgSpringPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
